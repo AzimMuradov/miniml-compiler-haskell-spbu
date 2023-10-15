97f1257ce1b2b791fa519bc194809a2b2e8299c1
Alexander Zadorozhnyy <66938442+Alexander-Zadorozhnyy@users.noreply.github.com>
2023-10-16 00:15:03 +0300

    Refactor `TypeInference` modules, add unit tests (#5)
    - Add the ability to specify the type of the returned value
    - Update type inference to be able to interact with func's returned type
    - Add type-inference tests
    
    ---------
    
    Co-authored-by: Azim Muradov <azim.muradov.dev@gmail.com>

--- test/Main.hs
+++ test/Main.hs
@@ -2,11 +2,12 @@ module Main where
 
 import qualified Parser.ParserTest as ParserTest
 import qualified System.Exit as Exit
-import Test.HUnit (Counts (failures), runTestTT)
+import Test.HUnit (Counts (failures), Test (TestList), runTestTT)
+import qualified TypeInference.TypeInferenceTest as TypeInferenceTest
 
 main :: IO ()
 main = do
-  result <- runTestTT ParserTest.tests
+  result <- runTestTT $ TestList [ParserTest.tests, TypeInferenceTest.tests]
   if failures result > 0
     then Exit.exitFailure
     else Exit.exitSuccess
