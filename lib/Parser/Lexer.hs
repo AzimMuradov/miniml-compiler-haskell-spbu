89938df5597bcb3a1463611cbda1a1b87a5afb36
Azim Muradov <azim.muradov.dev@gmail.com>
2023-10-15 22:16:57 +0300

    Refactor `Parser` modules, add unit tests and update docs (#4)
    - Refactor `Lexer`, `Parser` and `Ast`
    - Improve docs of `Ast`
    - Fix bug with identifiers and keywords collisions
    - Eliminate `fileP` and update grammar to support multiple `;;` per program
    - Change `[Expression]` to `Expression` in the "block" structures
    - Add unit tests for `Parser`
    - Add `--test-show-details=direct` for the test command in CI
    
    ---------
    
    Co-authored-by: Alexander Zadorozhnyy <alexander.zadorozhnyy@yandex.ru>

--- lib/Parser/Lexer.hs
+++ lib/Parser/Lexer.hs
@@ -2,107 +2,139 @@
 
 module Parser.Lexer where
 
-import Control.Applicative.Combinators (between)
-import Data.Text (Text)
+import Data.Text (Text, pack)
 import Data.Void (Void)
-import Text.Megaparsec (MonadParsec (..), Parsec, choice)
-import Text.Megaparsec.Char (space1)
+import Parser.Ast (Identifier)
+import Text.Megaparsec (MonadParsec (..), Parsec, between, choice, many, optional, (<|>))
+import Text.Megaparsec.Char (char, digitChar, letterChar, space1, string)
 import qualified Text.Megaparsec.Char.Lexer as L
 
+-- * Basic lexer parts
+
+-- | Parser monad.
 type Parser = Parsec Void Text
 
--- Comments
-lineComment :: Parser ()
-lineComment = L.skipLineComment "//"
-
-blockComment :: Parser ()
-blockComment = L.skipBlockComment "(*" "*)"
-
--- Space Consumers
+-- | Space consumer, parses whitespace and comments.
 sc :: Parser ()
-sc = L.space space1 lineComment blockComment
+sc = L.space space1 (L.skipLineComment "//") (L.skipBlockComment "(*" "*)")
 
--- Text Parsing Helpers
-
+-- | Lexeme, automatically parses trailing whitespace and comments.
 lexeme :: Parser a -> Parser a
 lexeme = L.lexeme sc
 
+-- | Symbol, automatically parses trailing whitespace and comments.
 symbol :: Text -> Parser Text
 symbol = L.symbol sc
 
--- Smart Choice
+-- * Symbols
+
+-- | Wraps the given parser with parenthesis.
+parens :: Parser a -> Parser a
+parens = between (symbol "(") (symbol ")")
+
+-- | Colon parser.
+colon :: Parser Text
+colon = symbol ":"
+
+-- | Double semicolon parser.
+semicolon2 :: Parser Text
+semicolon2 = symbol ";;"
+
+-- | Arrow parser.
+arrow :: Parser Text
+arrow = symbol "->"
+
+-- | Equality parser.
+eq :: Parser Text
+eq = symbol "="
+
+-- * Literals
+
+-- | Boolean literal parser.
+boolLitP :: Parser Bool
+boolLitP = True <$ idTrue <|> False <$ idFalse
+
+-- | Signed decimal integer literal parser.
+signedIntP :: Parser Integer
+signedIntP = L.signed (return ()) intP
+  where
+    intP = lexeme L.decimal
+
+-- * Identifiers and reserved
+
+-- ** Identifier
+
+identifierP :: Parser Identifier
+identifierP =
+  lexeme $
+    notFollowedBy reservedP *> do
+      first <- firstP
+      other <- many otherP
+      return $ pack $ first : other
+  where
+    firstP = letterChar <|> char '_'
+    otherP = firstP <|> digitChar
+
+reservedP :: Parser Text
+reservedP = choice [kwLet, kwRec, kwIn, kwIf, kwThen, kwElse, kwFun, idBool, idInt, idTrue, idFalse]
+
+reservedP' :: Text -> Parser Text
+reservedP' ident = lexeme $ string ident <* notFollowedBy (letterChar <|> char '_' <|> digitChar)
+
+-- ** Keywords
+
+-- @let@ keyword parser.
+kwLet :: Parser Text
+kwLet = reservedP' "let"
+
+-- @rec@ keyword parser.
+kwRec :: Parser Text
+kwRec = reservedP' "rec"
+
+-- @in@ keyword parser.
+kwIn :: Parser Text
+kwIn = reservedP' "in"
+
+-- @if@ keyword parser.
+kwIf :: Parser Text
+kwIf = reservedP' "if"
+
+-- @then@ keyword parser.
+kwThen :: Parser Text
+kwThen = reservedP' "then"
+
+-- @else@ keyword parser.
+kwElse :: Parser Text
+kwElse = reservedP' "else"
+
+-- @fun@ keyword parser.
+kwFun :: Parser Text
+kwFun = reservedP' "fun"
+
+-- ** Predeclared identifiers
+
+-- | @bool@ identifier parser.
+idBool :: Parser Text
+idBool = reservedP' "bool"
+
+-- | @int@ identifier parser.
+idInt :: Parser Text
+idInt = reservedP' "int"
+
+-- | @true@ identifier parser.
+idTrue :: Parser Text
+idTrue = reservedP' "true"
+
+-- | @false@ identifier parser.
+idFalse :: Parser Text
+idFalse = reservedP' "false"
+
+-- * Utilities
+
+-- ** Backtracking support
 
 choice' :: (Foldable f, MonadParsec e s m, Functor f) => f (m a) -> m a
 choice' x = choice $ try <$> x
 
--- Symbols
-
-parens :: Parser a -> Parser a
-parens = between (symbol "(") (symbol ")")
-
-colon :: Parser Text
-colon = symbol ":"
-
-semicolon2 :: Parser Text
-semicolon2 = symbol ";;"
-
-arrow :: Parser Text
-arrow = symbol "->"
-
-comma :: Parser Text
-comma = symbol ","
-
-eq :: Parser Text
-eq = symbol "="
-
--- Reserved Parser
-reservedP :: Parser Text
-reservedP = choice [wTrue, wFalse, wBool, wInt, kIf, kThen, kElse, kLet, kRec, kIn, kFun]
-
--- Reserved Words
-
--- True Parser
-wTrue :: Parser Text
-wTrue = symbol "true"
-
--- False Parser
-wFalse :: Parser Text
-wFalse = symbol "false"
-
--- Bool Parser
-wBool :: Parser Text
-wBool = symbol "bool"
-
--- Int Parser
-wInt :: Parser Text
-wInt = symbol "int"
-
--- Keyword Parsers
-
--- ifParser
-kIf :: Parser Text
-kIf = symbol "if"
-
--- thenParser
-kThen :: Parser Text
-kThen = symbol "then"
-
--- elseParser
-kElse :: Parser Text
-kElse = symbol "else"
-
--- letParser
-kLet :: Parser Text
-kLet = symbol "let"
-
--- recParser
-kRec :: Parser Text
-kRec = symbol "rec"
-
--- inParser
-kIn :: Parser Text
-kIn = symbol "in"
-
--- inParser
-kFun :: Parser Text
-kFun = symbol "fun"
+optional' :: (MonadParsec e s f) => f a -> f (Maybe a)
+optional' = optional . try
---
+++ test/Parser/ParserTest.hs
@@ -0,0 +1,160 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Parser.ParserTest (tests) where
+
+import Data.Text (Text)
+import Parser.Ast
+import Parser.Parser (parse, programP)
+import Test.HUnit (Test (TestList), (~:), (~=?))
+
+tests :: Test
+tests =
+  "parser"
+    ~: TestList
+      [ test0,
+        test1,
+        test2,
+        test3,
+        test4,
+        test5,
+        test6,
+        test7,
+        test8
+      ]
+
+-- Tests
+
+test0 :: Test
+test0 =
+  "[let a = 7]"
+    ~: do
+      let expected = Just $ Program [StmtVarDecl (VarDecl ("a", Nothing) (ExprValue (ValInt 7)))]
+      let actual = parseProgram "let a = 7"
+
+      expected ~=? actual
+
+test1 :: Test
+test1 =
+  "[let = 7]"
+    ~: do
+      let expected = Nothing
+      let actual = parseProgram "let = 7"
+
+      expected ~=? actual
+
+test2 :: Test
+test2 =
+  "[let a]"
+    ~: do
+      let expected = Nothing
+      let actual = parseProgram "let a"
+
+      expected ~=? actual
+
+test3 :: Test
+test3 =
+  "[leta = 7]"
+    ~: do
+      let expected = Just $ Program [StmtExpr (ExprOperations (ComparisonOp (EqOp {cL = ExprIdentifier "leta", cR = ExprValue (ValInt 7)})))]
+      let actual = parseProgram "leta = 7"
+
+      expected ~=? actual
+
+test4 :: Test
+test4 =
+  "[let a = 4 let b = 8]"
+    ~: do
+      let expected =
+            Just $
+              Program
+                [ StmtVarDecl (VarDecl ("a", Nothing) (ExprValue (ValInt 4))),
+                  StmtVarDecl (VarDecl ("b", Nothing) (ExprValue (ValInt 8)))
+                ]
+      let actual = parseProgram "let a = 4 let b = 8"
+
+      expected ~=? actual
+
+test5 :: Test
+test5 =
+  "[let a = 4\nlet b = 8]"
+    ~: do
+      let expected =
+            Just $
+              Program
+                [ StmtVarDecl (VarDecl ("a", Nothing) (ExprValue (ValInt 4))),
+                  StmtVarDecl (VarDecl ("b", Nothing) (ExprValue (ValInt 8)))
+                ]
+      let actual = parseProgram "let a = 4\nlet b = 8"
+
+      expected ~=? actual
+
+test6 :: Test
+test6 =
+  "[let a = 4;;let b = 8]"
+    ~: do
+      let expected =
+            Just $
+              Program
+                [ StmtVarDecl (VarDecl ("a", Nothing) (ExprValue (ValInt 4))),
+                  StmtVarDecl (VarDecl ("b", Nothing) (ExprValue (ValInt 8)))
+                ]
+      let actual = parseProgram "let a = 4;;let b = 8"
+
+      expected ~=? actual
+
+test7 :: Test
+test7 =
+  "[let f a = a * a f 4]"
+    ~: do
+      let expected =
+            Just $
+              Program
+                [ StmtFunDecl
+                    ( FunDecl
+                        "f"
+                        ( Fun
+                            [("a", Nothing)]
+                            ( ExprOperations
+                                ( ArithmeticOp
+                                    ( MulOp
+                                        { aL = ExprIdentifier "a",
+                                          aR =
+                                            ExprApplication
+                                              (ExprApplication (ExprIdentifier "a") (ExprIdentifier "f"))
+                                              (ExprValue (ValInt 4))
+                                        }
+                                    )
+                                )
+                            )
+                        )
+                    )
+                ]
+      let actual = parseProgram "let f a = a * a f 4"
+
+      expected ~=? actual
+
+test8 :: Test
+test8 =
+  "[let f a = a * a;;f 4]"
+    ~: do
+      let expected =
+            Just $
+              Program
+                [ StmtFunDecl
+                    ( FunDecl
+                        "f"
+                        ( Fun
+                            [("a", Nothing)]
+                            (ExprOperations (ArithmeticOp (MulOp {aL = ExprIdentifier "a", aR = ExprIdentifier "a"})))
+                        )
+                    ),
+                  StmtExpr (ExprApplication (ExprIdentifier "f") (ExprValue (ValInt 4)))
+                ]
+      let actual = parseProgram "let f a = a * a;;f 4"
+
+      expected ~=? actual
+
+-- Utils
+
+parseProgram :: Text -> Maybe Program
+parseProgram = parse programP
