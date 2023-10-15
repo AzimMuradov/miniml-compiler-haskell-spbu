fbd80e2b48dc70bfd922e51b4ec730a91033cedf
Azim Muradov <azim.muradov.dev@gmail.com>
2023-10-15 19:27:07 +0300

    Improve docs of `Ast`

--- lib/Parser/Ast.hs
+++ lib/Parser/Ast.hs
@@ -1,11 +1,15 @@
 {-# LANGUAGE DuplicateRecordFields #-}
 
+-- | Contains all AST elements, all of these produced by the [Parser]("Parser.Parser") module.
 module Parser.Ast where
 
 import Data.Text (Text)
 
+--------------------------------------------------------Program---------------------------------------------------------
+
 -- * Main program
 
+-- | The head of the AST.
 newtype Program = Program [Statement]
   deriving (Show, Eq)
 
@@ -36,21 +40,34 @@ data FunDecl = FunDecl Identifier Fun deriving (Show, Eq)
 -- TODO : Add docs
 data RecFunDecl = RecFunDecl Identifier Fun deriving (Show, Eq)
 
+---------------------------------------------------------Types----------------------------------------------------------
+
+-- * Types
+
+-- | Type.
+data Type
+  = TBool
+  | TInt
+  | TFun Type Type
+  deriving (Show, Eq)
+
 ------------------------------------------------------Expressions-------------------------------------------------------
 
 -- * Expressions
 
 -- | Expression.
 data Expression
-  = -- | ( "x", "y" )
+  = -- | Identifier expression, see 'Identifier'.
     ExprIdentifier Identifier
-  | -- | ( 5, 7.4, fun x y = x + y )
+  | -- | Value expression, see 'Value'.
     ExprValue Value
-  | -- | ( +, - )
+  | -- | Operation expression, see 'Operations'.
     ExprOperations Operations
   | -- | ( f 6, (fun x y = x + y) 5 6 )
     ExprApplication Expression Expression
-  | -- | ( if cond then e1 else e2 )
+  | -- | If-else expression.
+    --
+    -- > if condition then expr1 else expr2
     ExprIf Expression [Expression] [Expression]
   | -- | ( let x = 4 in ... )
     ExprLetInV (Identifier, Maybe Type) [Expression] [Expression]
@@ -60,78 +77,77 @@ data Expression
     ExprLetRecInF Identifier Fun [Expression]
   deriving (Show, Eq)
 
--- * Types
+-- ** Values
 
--- ** BasicTypes
-
--- | Type.
-data Type
-  = TBool
-  | TInt
-  | TFun Type Type
-  deriving (Show, Eq)
-
--- * Values
-
--- | Value.
+-- | Literal or function value.
 data Value
-  = ValBool Bool
-  | ValInt Integer
-  | ValFun Fun
+  = -- | Boolean literal value (e.g., @true@, @false@).
+    ValBool Bool
+  | -- | Int literal value (e.g., @4@, @-15@).
+    ValInt Integer
+  | -- | Function value, see 'Fun'.
+    ValFun Fun
   deriving (Show, Eq)
 
--- | ( fun x y -> x + y )
+-- | Function representation without name.
+--
+-- > fun x -> true
+--
+-- > fun x y -> x + y
 data Fun = Fun [(Identifier, Maybe Type)] [Expression]
   deriving (Show, Eq)
 
--- * Operators
+-- ** Operations
 
--- TODO : Add docs
+-- | Operation.
 data Operations
-  = BooleanOp BooleanOp
-  | -- | ( not )
+  = -- | Boolean operation, see 'BooleanOp'.
+    BooleanOp BooleanOp
+  | -- | Negation operation (@not a@), works only for @bool@.
     NotOp Expression
-  | ArithmeticOp ArithmeticOp
-  | ComparisonOp ComparisonOp
+  | -- | Arithmetic operation, see 'ArithmeticOp'.
+    ArithmeticOp ArithmeticOp
+  | -- | Comparison operation, see 'ComparisonOp'.
+    ComparisonOp ComparisonOp
   deriving (Show, Eq)
 
--- TODO : Add docs
+-- | Boolean operation.
 data BooleanOp
-  = -- | ( && )
+  = -- | And operation (@a && b@).
     AndOp {bL :: Expression, bR :: Expression}
-  | -- | ( || )
+  | -- | Or operation (@a || b@).
     OrOp {bL :: Expression, bR :: Expression}
   deriving (Show, Eq)
 
--- TODO : Add docs
+-- | Arithmetic operation.
 data ArithmeticOp
-  = -- | ( + )
+  = -- | Addition operation (@a + b@).
     PlusOp {aL :: Expression, aR :: Expression}
-  | -- | ( - )
+  | -- | Subtraction operation (@a - b@).
     MinusOp {aL :: Expression, aR :: Expression}
-  | -- | ( * )
+  | -- | Multiplication operation (@a * b@).
     MulOp {aL :: Expression, aR :: Expression}
-  | -- | ( / )
+  | -- | Division operation (@a / b@).
     DivOp {aL :: Expression, aR :: Expression}
   deriving (Show, Eq)
 
--- TODO : Add docs
+-- | Comparison operation.
 data ComparisonOp
-  = -- | ( = )
+  = -- | Equality check (@a = b@).
     EqOp {cL :: Expression, cR :: Expression}
-  | -- | ( <> )
+  | -- | Non-equality check (@a <> b@).
     NeOp {cL :: Expression, cR :: Expression}
-  | -- | ( < )
+  | -- | Less than (@a < b@).
     LtOp {cL :: Expression, cR :: Expression}
-  | -- | ( <= )
+  | -- | Less than or equal operator (@a <= b@).
     LeOp {cL :: Expression, cR :: Expression}
-  | -- | ( > )
+  | -- | More than operator (@a > b@).
     MtOp {cL :: Expression, cR :: Expression}
-  | -- | ( >= )
+  | -- | More than or equal operator (@a >= b@).
     MeOp {cL :: Expression, cR :: Expression}
   deriving (Show, Eq)
 
--- * Identifier
+-- ** Identifier
 
--- | Any valid identifier.
+-- | Any valid identifier (e.g., @he42llo@, @_42@).
 type Identifier = Text
