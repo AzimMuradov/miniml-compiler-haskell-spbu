c6453778ffd9e2a027641a1c615a5d07f8d24aff
Alexander Zadorozhnyy <alexander.zadorozhnyy@yandex.ru>
2023-10-29 22:03:17 +0300

    Add print_int and print_bool to stdlib

--- lib/StdLib.hs
+++ lib/StdLib.hs
@@ -5,7 +5,13 @@ module StdLib (stdDeclarations) where
 import Parser.Ast
 
 stdDeclarations :: [Statement]
-stdDeclarations = [notDecl]
+stdDeclarations = [notDecl, printInt, printBool]
 
 notDecl :: Statement
 notDecl = StmtStdDecl "not" (TFun TBool TBool)
+
+printInt :: Statement
+printInt = StmtStdDecl "print_int" (TFun TInt TUnit)
+
+printBool :: Statement
+printBool = StmtStdDecl "print_bool" (TFun TBool TUnit)
