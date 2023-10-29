0c7354e274d2e0a027e4a4fa9b922ba6d76dc050
Azim Muradov <azim.muradov.dev@gmail.com>
2023-10-30 02:10:30 +0300

    Simplify `inferFun`

--- lib/TypeInference/TypeInference.hs
+++ lib/TypeInference/TypeInference.hs
@@ -9,7 +9,7 @@
 module TypeInference.TypeInference where
 
 import Control.Monad.Except
-import Data.List.NonEmpty (NonEmpty, toList)
+import Data.List.NonEmpty (toList)
 import Data.Maybe
 import Parser.Ast
 import TypeInference.HindleyMilner
@@ -33,13 +33,13 @@ inferStatements' ((StmtUserDecl (DeclVar (ident, t) body)) : xs) _ = do
   vType <- maybe (return res) ((=:=) res <$> fromTypeToUType) t
   pvType <- generalize vType
   withBinding ident pvType (inferStatements' xs $ return vType)
-inferStatements' ((StmtUserDecl (DeclFun ident (Fun args restype body))) : xs) _ = do
-  res <- inferFun args restype body
+inferStatements' ((StmtUserDecl (DeclFun ident fun)) : xs) _ = do
+  res <- inferFun fun
   withBinding ident (Forall [] res) (inferStatements' xs $ return res)
-inferStatements' ((StmtUserDecl (DeclRecFun ident (Fun args restype body))) : xs) _ = do
+inferStatements' ((StmtUserDecl (DeclRecFun ident fun)) : xs) _ = do
   preT <- fresh
-  next <- withBinding ident (Forall [] preT) $ inferFun args restype body
-  after <- withBinding ident (Forall [] next) $ inferFun args restype body
+  next <- withBinding ident (Forall [] preT) $ inferFun fun
+  after <- withBinding ident (Forall [] next) $ inferFun fun
   withBinding ident (Forall [] after) (inferStatements' xs $ return next)
 inferStatements' ((StmtStdDecl (StdDecl ident t)) : xs) _ = do
   let t' = fromTypeToUType t
@@ -51,7 +51,7 @@ inferSingle (ExprIdentifier x) = lookup (Var x)
 inferSingle (ExprValue ValUnit) = return UTyUnit
 inferSingle (ExprValue (ValBool _)) = return UTyBool
 inferSingle (ExprValue (ValInt _)) = return UTyInt
-inferSingle (ExprValue (ValFun (Fun xs restype body))) = inferFun xs restype body
+inferSingle (ExprValue (ValFun fun)) = inferFun fun
 inferSingle (ExprBinaryOperation op lhs rhs) = do
   utLhs <- inferSingle lhs
   utRhs <- inferSingle rhs
@@ -86,20 +86,20 @@ inferSingle (ExprLetIn (DeclVar (x, Nothing) xdef) body) = do
   ty <- inferSingle xdef
   pty <- generalize ty
   withBinding x pty $ inferSingle body
-inferSingle (ExprLetIn (DeclFun f (Fun args restype fbody)) lbody) = do
-  fdef <- inferFun args restype fbody
+inferSingle (ExprLetIn (DeclFun f fun) lbody) = do
+  fdef <- inferFun fun
   pfdef <- generalize fdef
   withBinding f pfdef $ inferSingle lbody
-inferSingle (ExprLetIn (DeclRecFun f (Fun args restype fbody)) lbody) = do
+inferSingle (ExprLetIn (DeclRecFun f fun) lbody) = do
   preT <- fresh
-  next <- withBinding f (Forall [] preT) $ inferFun args restype fbody
-  after <- withBinding f (Forall [] next) $ inferFun args restype fbody
+  next <- withBinding f (Forall [] preT) $ inferFun fun
+  after <- withBinding f (Forall [] next) $ inferFun fun
   inferedBlock <- withBinding f (Forall [] next) (inferSingle lbody)
   pfdef <- generalize after
   withBinding f pfdef (return inferedBlock)
 
-inferFun :: NonEmpty (Identifier, Maybe Type) -> Maybe Type -> Expression -> Infer UType
-inferFun args restype body = inferFun' $ toList args
+inferFun :: Fun -> Infer UType
+inferFun (Fun args restype body) = inferFun' $ toList args
   where
     inferFun' args' = case args' of
       [] -> do
