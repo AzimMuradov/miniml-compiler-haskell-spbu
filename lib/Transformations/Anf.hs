c674500a67e8364f70f249f6cada99d928a69cfe
Azim Muradov <azim.muradov.dev@gmail.com>
2023-11-03 10:35:44 +0300

    Add `Anf`

---
+++ lib/Transformations/Anf.hs
@@ -0,0 +1,33 @@
+module Transformations.Anf where
+
+import Data.List.NonEmpty (NonEmpty)
+import Data.Text (Text)
+
+newtype Program = Program [Statement]
+  deriving (Show, Eq)
+
+data Statement
+  = StmtDecl Identifier Expression
+  | StmtExpr Expression
+  deriving (Show, Eq)
+
+data AtomicExpression
+  = AtomExprIdentifier Identifier
+  | AtomExprUnit
+  | AtomExprBool Bool
+  | AtomExprInt Integer
+  | AtomExprClosure (NonEmpty Identifier) Expression
+  deriving (Show, Eq)
+
+data ComplexExpression
+  = CompExprApp AtomicExpression AtomicExpression
+  | CompExprIte AtomicExpression Expression Expression
+  deriving (Show, Eq)
+
+data Expression
+  = ExprAtomExpr AtomicExpression
+  | ExprCompExpr ComplexExpression
+  | ExprLetIn Identifier Expression Expression
+  deriving (Show, Eq)
+
+type Identifier = Text
