3eb6967bdf814c27d40b62f944f1125a682383da
Azim Muradov <azim.muradov.dev@gmail.com>
2023-11-04 17:15:48 +0300

    Fix bug with binding order

--- lib/Transformations/TypelessAstToAnf.hs
+++ lib/Transformations/TypelessAstToAnf.hs
@@ -17,51 +17,45 @@ normalizeProgram :: Program -> CntState Anf.Program
 normalizeProgram (Program stmts) = Anf.Program <$> mapM normalizeStatement stmts
 
 normalizeStatement :: Statement -> CntState Anf.Statement
-normalizeStatement (StmtDecl name value) = Anf.StmtDecl name <$> normalizeTerm' value
-normalizeStatement (StmtExpr expr) = Anf.StmtExpr <$> normalizeTerm' expr
+normalizeStatement (StmtDecl name value) = Anf.StmtDecl name <$> normalizeExpr value
+normalizeStatement (StmtExpr expr) = Anf.StmtExpr <$> normalizeExpr expr
 
-normalizeExpr :: Expression -> CpsWithCnt Anf.Expression Anf.Expression
+normalizeExpr :: Expression -> CntState Anf.Expression
 normalizeExpr (ExprIdentifier name) = returnAtom $ Anf.AtomIdentifier name
 normalizeExpr (ExprValue value) = case value of
   ValUnit -> returnAtom Anf.AtomUnit
   ValBool bool -> returnAtom $ Anf.AtomBool bool
   ValInt int -> returnAtom $ Anf.AtomInt int
-  ValFun params body -> Anf.ExprAtom . Anf.AtomClosure params <$> normalizeTerm body
-normalizeExpr (ExprApplication f args) = do
+  ValFun params body -> Anf.ExprAtom . Anf.AtomClosure params <$> normalizeExpr body
+normalizeExpr (ExprApplication f args) = evalContT $ do
   f' <- normalizeName f
   args' <- mapM normalizeName args
   returnComplex $ Anf.CompApp f' args'
-normalizeExpr (ExprIte c t e) = do
+normalizeExpr (ExprIte c t e) = evalContT $ do
   c' <- normalizeName c
-  t' <- normalizeTerm t
-  e' <- normalizeTerm e
+  t' <- lift $ normalizeExpr t
+  e' <- lift $ normalizeExpr e
   returnComplex $ Anf.CompIte c' t' e'
 normalizeExpr (ExprLetIn (name, value) expr) = do
   value' <- normalizeExpr value
   expr' <- normalizeExpr expr
   return $ Anf.ExprLetIn name value' expr'
 
-returnAtom :: Anf.AtomicExpression -> CpsWithCnt r Anf.Expression
+returnAtom :: Anf.AtomicExpression -> CntState Anf.Expression
 returnAtom = return . Anf.ExprAtom
 
-returnComplex :: Anf.ComplexExpression -> CpsWithCnt r Anf.Expression
+returnComplex :: Anf.ComplexExpression -> CpsWithCnt Anf.Expression Anf.Expression
 returnComplex = return . Anf.ExprComp
-
-normalizeTerm :: Expression -> CpsWithCnt r Anf.Expression
-normalizeTerm expr = lift $ normalizeTerm' expr
-
-normalizeTerm' :: Expression -> CntState Anf.Expression
-normalizeTerm' expr = evalContT $ normalizeExpr expr
 
 normalizeName :: Expression -> CpsWithCnt Anf.Expression Anf.AtomicExpression
 normalizeName expr = do
-  expr' <- normalizeExpr expr
+  expr' <- lift $ normalizeExpr expr
   case expr' of
     Anf.ExprAtom atom -> return atom
     _ -> do
       name <- genName
       mapContT
-        (\expr'' -> Anf.ExprLetIn name expr' <$> expr'')
+        (\e -> Anf.ExprLetIn name expr' <$> e)
         (return $ Anf.AtomIdentifier name)
 
 genName :: CpsWithCnt r Text
