0cc1aa0bd368561d7299793490a29d1de7eebfd0
Alexander Zadorozhnyy <alexander.zadorozhnyy@yandex.ru>
2023-11-13 21:29:21 +0300

    Fix `AnfPrettyPrinter` and refactor code (#21)
    - Fix bug with args in `AnfPrettyPrinter`
    - Add CPS FacRec test
    
    ---------
    
    Co-authored-by: Azim Muradov <azim.muradov.dev@gmail.com>

--- lib/Transformations/AnfPrettyPrinter.hs
+++ lib/Transformations/AnfPrettyPrinter.hs
@@ -11,24 +11,13 @@ prettyPrint' :: [Statement] -> String
 prettyPrint' stmts = unlines $ map prettyStmt stmts
 
 prettyStmt :: Statement -> String
-prettyStmt (StmtDecl name expr) =
-  "let " <> prettyIdentifier name <> case expr of
-    (ExprAtom (AtomClosure _ _)) -> " " <> prettyExpr expr
-    _ -> " = " <> prettyExpr expr
-prettyStmt (StmtExpr expr) = prettyExpr expr
+prettyStmt (StmtDecl name expr) = doubleSemicolon $ unwords ["let", prettyIdentifier name, "=", prettyExpr expr]
+prettyStmt (StmtExpr expr) = doubleSemicolon $ prettyExpr expr
 
 prettyExpr :: Expression -> String
 prettyExpr (ExprAtom aexpr) = prettyAtomic aexpr
 prettyExpr (ExprComp cexpr) = prettyComplex cexpr
-prettyExpr (ExprLetIn name value expr) =
-  concat
-    [ "let ",
-      prettyIdentifier name,
-      " = ",
-      prettyExpr value,
-      " in ",
-      prettyExpr expr
-    ]
+prettyExpr (ExprLetIn name value expr) = unwords ["let", prettyIdentifier name, "=", prettyExpr value, "in", prettyExpr expr]
 
 prettyAtomic :: AtomicExpression -> String
 prettyAtomic aexpr = case aexpr of
@@ -36,23 +25,18 @@ prettyAtomic aexpr = case aexpr of
   AtomUnit -> parens ""
   AtomBool value -> if value then "true" else "false"
   AtomInt value -> show value
-  AtomClosure args expr -> unwords (map prettyIdentifier $ toList args) <> " = " <> prettyExpr expr
+  AtomClosure args expr -> parens $ "fun " <> unwords (map prettyIdentifier $ toList args) <> " -> " <> prettyExpr expr
 
 prettyComplex :: ComplexExpression -> String
 prettyComplex cexpr = case cexpr of
-  CompApp f args -> parens $ prettyAtomic f <> unwords (map (\x -> " " <> prettyAtomic x) $ toList args)
-  CompIte c t e ->
-    concat
-      [ "if ",
-        prettyAtomic c,
-        " then ",
-        prettyExpr t,
-        " else ",
-        prettyExpr e
-      ]
+  CompApp f args -> parens $ prettyAtomic f <> " " <> unwords (map prettyAtomic $ toList args)
+  CompIte c t e -> unwords ["if", prettyAtomic c, "then", prettyExpr t, "else", prettyExpr e]
 
 prettyIdentifier :: Identifier -> String
 prettyIdentifier ident = "`" <> unpack ident <> "`"
 
 parens :: String -> String
 parens val = "(" <> val <> ")"
+
+doubleSemicolon :: String -> String
+doubleSemicolon val = val <> ";;"
