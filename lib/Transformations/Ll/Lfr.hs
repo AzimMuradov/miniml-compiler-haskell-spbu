7c0e6f50c63f6f159f22217d644cd07df70462db
Azim Muradov <azim.muradov.dev@gmail.com>
2023-12-06 09:32:40 +0300

    Add lambda lifting
    - Add lambda-free representation (`Lfr`)
    - Add lambda lifting (`Ll`)

---
+++ lib/Transformations/Ll/Lfr.hs
@@ -0,0 +1,27 @@
+module Transformations.Ll.Lfr where
+
+import Trees.Common
+
+data Program = Program
+  { topLevelDeclarations :: [TopLevelDeclaration],
+    idCounter :: Int
+  }
+  deriving (Show, Eq)
+
+data TopLevelDeclaration
+  = TopLevelVarDecl VarDeclaration
+  | TopLevelFunDecl Identifier' [Identifier'] Expression
+  deriving (Show, Eq)
+
+data VarDeclaration = VarDecl Identifier' Expression
+  deriving (Show, Eq)
+
+data Expression
+  = ExprId Identifier'
+  | ExprVal Value
+  | ExprBinOp BinaryOperator Expression Expression
+  | ExprUnOp UnaryOperator Expression
+  | ExprApp Expression Expression
+  | ExprIte Expression Expression Expression
+  | ExprLetIn [VarDeclaration] Expression
+  deriving (Show, Eq)
