43f4f8f5bce69843636e99d3a3ba850887d28456
Azim Muradov <azim.muradov.dev@gmail.com>
2024-01-02 21:48:41 +0300

    Refactor `Ll`

--- lib/Transformations/Ll/Lfr.hs
+++ lib/Transformations/Ll/Lfr.hs
@@ -2,12 +2,12 @@ module Transformations.Ll.Lfr where
 
 import Trees.Common
 
-data Program = Program [TopLevelDeclaration] IdCnt
+data Program = Program [GlobalDeclaration] IdCnt
   deriving (Show, Eq)
 
-data TopLevelDeclaration
-  = TopLevelVarDecl VarDeclaration
-  | TopLevelFunDecl Identifier' [Identifier'] Expression
+data GlobalDeclaration
+  = GlobVarDecl VarDeclaration
+  | GlobFunDecl Identifier' [Identifier'] Expression
   deriving (Show, Eq)
 
 data VarDeclaration = VarDecl Identifier' Expression
