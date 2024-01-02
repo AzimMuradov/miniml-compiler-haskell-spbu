767db98ed395f649f7259e055dd26ea4df837c8e
Azim Muradov <azim.muradov.dev@gmail.com>
2024-01-02 21:48:57 +0300

    Refactor `AnfGen`

--- lib/Transformations/Anf/AnfGen.hs
+++ lib/Transformations/Anf/AnfGen.hs
@@ -1,98 +1,100 @@
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE TupleSections #-}
 
-module Transformations.Anf.AnfGen (astToAnf) where
+module Transformations.Anf.AnfGen (genAnf) where
 
 import Control.Monad.Cont (ContT, mapContT)
-import Control.Monad.State (MonadTrans (lift), State, evalState, get, modify)
+import Control.Monad.State (MonadState, MonadTrans (lift), State, evalState, get, modify)
 import Control.Monad.Trans.Cont (evalContT)
 import Data.Text (pack)
-import qualified Parser.Ast as Ast
 import qualified Transformations.Anf.Anf as Anf
-import Transformations.Cc.Cc (ccAst)
 import qualified Transformations.Ll.Lfr as Lfr
-import Transformations.Ll.Ll (llAst)
-import Transformations.Relabeler.RelabelVars (relabelAst)
-import Transformations.Simplification.SimplifyAst (simplifyAst)
 import qualified Trees.Common as Common
 
-astToAnf :: Ast.Program -> Anf.Program
-astToAnf = lfrToAnf . llAst . ccAst . relabelAst . simplifyAst
+-- * ANF Generator
 
-lfrToAnf :: Lfr.Program -> Anf.Program
-lfrToAnf (Lfr.Program tlDecls cnt) = Anf.Program $ evalState (mapM normalizeDecl tlDecls) cnt
+genAnf :: Lfr.Program -> Anf.Program
+genAnf (Lfr.Program gDecls cnt) =
+  let gDecls' = evalState (mapM genDecl gDecls) cnt
+   in Anf.Program gDecls'
 
-type CpsWithCnt r a = ContT r CntState a
+-- * Internal
 
-type CntState = State IdCnt
+-- * ANF Normalizer Continuation & Generator State
 
-type IdCnt = Int
+type NormCont r a = ContT r AnfGenState a
 
-normalizeDecl :: Lfr.TopLevelDeclaration -> CntState Anf.GlobalDeclaration
-normalizeDecl (Lfr.TopLevelVarDecl (Lfr.VarDecl name value)) = Anf.GlobVarDecl name <$> normalizeExpr value
-normalizeDecl (Lfr.TopLevelFunDecl name params body) = Anf.GlobFunDecl name params <$> normalizeExpr body
+type AnfGenState = State Env
 
-normalizeExpr :: Lfr.Expression -> CntState Anf.Expression
-normalizeExpr (Lfr.ExprId name) = returnAtom $ Anf.AtomId name
-normalizeExpr (Lfr.ExprVal value) = returnAtom $ case value of
+type Env = Common.IdCnt
+
+-- ** ANF Generators
+
+genDecl :: Lfr.GlobalDeclaration -> AnfGenState Anf.GlobalDeclaration
+genDecl (Lfr.GlobVarDecl (Lfr.VarDecl ident value)) = Anf.GlobVarDecl ident <$> genExpr value
+genDecl (Lfr.GlobFunDecl ident params body) = Anf.GlobFunDecl ident params <$> genExpr body
+
+genExpr :: Lfr.Expression -> AnfGenState Anf.Expression
+genExpr (Lfr.ExprId ident) = returnAtom $ Anf.AtomId ident
+genExpr (Lfr.ExprVal val) = returnAtom $ case val of
   Common.ValUnit -> Anf.AtomUnit
   Common.ValBool bool -> Anf.AtomBool bool
   Common.ValInt int -> Anf.AtomInt int
-normalizeExpr (Lfr.ExprBinOp op lhs rhs) = evalContT $ do
-  lhs' <- normalizeName lhs
-  rhs' <- normalizeName rhs
-  returnAtom' $ Anf.AtomBinOp op lhs' rhs'
-normalizeExpr (Lfr.ExprUnOp op x) = evalContT $ do
-  x' <- normalizeName x
-  returnAtom' $ Anf.AtomUnOp op x'
-normalizeExpr (Lfr.ExprApp f arg) = evalContT $ do
-  f' <- normalizeName' f
-  arg' <- normalizeName arg
+genExpr (Lfr.ExprBinOp op lhs rhs) = evalContT $ do
+  lhs' <- normalizeToAtom lhs
+  rhs' <- normalizeToAtom rhs
+  returnAtom $ Anf.AtomBinOp op lhs' rhs'
+genExpr (Lfr.ExprUnOp op x) = evalContT $ do
+  x' <- normalizeToAtom x
+  returnAtom $ Anf.AtomUnOp op x'
+genExpr (Lfr.ExprApp f arg) = evalContT $ do
+  f' <- normalizeToId f
+  arg' <- normalizeToAtom arg
   returnComplex $ Anf.CompApp f' arg'
-normalizeExpr (Lfr.ExprIte c t e) = evalContT $ do
-  c' <- normalizeName c
-  t' <- lift $ normalizeExpr t
-  e' <- lift $ normalizeExpr e
+genExpr (Lfr.ExprIte c t e) = evalContT $ do
+  c' <- normalizeToAtom c
+  t' <- lift $ genExpr t
+  e' <- lift $ genExpr e
   returnComplex $ Anf.CompIte c' t' e'
-normalizeExpr (Lfr.ExprLetIn decls expr) = do
-  decls' <- normalizeVarDecl decls
-  expr' <- normalizeExpr expr
-  return $ Anf.ExprLetIn decls' expr'
-  where
-    normalizeVarDecl (Lfr.VarDecl name def) = (name,) <$> normalizeExpr def
+genExpr (Lfr.ExprLetIn (Lfr.VarDecl ident value) expr) = do
+  varDecl' <- (ident,) <$> genExpr value
+  expr' <- genExpr expr
+  return $ Anf.ExprLetIn varDecl' expr'
 
-returnAtom :: Anf.AtomicExpression -> CntState Anf.Expression
+returnAtom :: MonadState Env m => Anf.AtomicExpression -> m Anf.Expression
 returnAtom = return . Anf.ExprAtom
 
-returnAtom' :: Anf.AtomicExpression -> CpsWithCnt Anf.Expression Anf.Expression
-returnAtom' = return . Anf.ExprAtom
-
-returnComplex :: Anf.ComplexExpression -> CpsWithCnt Anf.Expression Anf.Expression
+returnComplex :: MonadState Env m => Anf.ComplexExpression -> m Anf.Expression
 returnComplex = return . Anf.ExprComp
 
-normalizeName :: Lfr.Expression -> CpsWithCnt Anf.Expression Anf.AtomicExpression
-normalizeName expr = do
-  expr' <- lift $ normalizeExpr expr
+-- ** Normalizers
+
+normalizeToAtom :: Lfr.Expression -> NormCont Anf.Expression Anf.AtomicExpression
+normalizeToAtom expr = do
+  expr' <- lift $ genExpr expr
   case expr' of
     Anf.ExprAtom atom -> return atom
     _ -> do
-      name <- lift genName
+      ident <- lift genId
       mapContT
-        (\e -> Anf.ExprLetIn (name, expr') <$> e)
-        (return $ Anf.AtomId name)
+        (\e -> Anf.ExprLetIn (ident, expr') <$> e)
+        (return $ Anf.AtomId ident)
 
-normalizeName' :: Lfr.Expression -> CpsWithCnt Anf.Expression Common.Identifier'
-normalizeName' expr = do
-  expr' <- lift $ normalizeExpr expr
+normalizeToId :: Lfr.Expression -> NormCont Anf.Expression Common.Identifier'
+normalizeToId expr = do
+  expr' <- lift $ genExpr expr
   case expr' of
     Anf.ExprAtom (Anf.AtomId ident) -> return ident
     _ -> do
-      name <- lift genName
+      ident <- lift genId
       mapContT
-        (\e -> Anf.ExprLetIn (name, expr') <$> e)
-        (return name)
+        (\e -> Anf.ExprLetIn (ident, expr') <$> e)
+        (return ident)
 
-genName :: CntState Common.Identifier'
-genName = do
+-- ** Identifier Generation
+
+genId :: AnfGenState Common.Identifier'
+genId = do
   cnt <- get
   modify (+ 1)
   return $ Common.Gen cnt $ pack "anf"
