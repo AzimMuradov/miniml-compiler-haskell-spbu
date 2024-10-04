ed921ccf9f4dff5a1777be5689c25c955ceac39d
Azim Muradov <azim.muradov.dev@gmail.com>
2024-10-04 20:14:20 +0300

    Rename "Asm" to "Rv"

--- lib/CodeGen/RiscV/AsmGen.hs
+++ lib/CodeGen/RiscV/AsmGen.hs
@@ -6,7 +6,7 @@
 module CodeGen.RiscV.AsmGen (ppRiscVAsm) where
 
 import CodeGen.Module (Module (..))
-import qualified CodeGen.RiscV.Lib as Asm
+import qualified CodeGen.RiscV.Lib as Rv
 import Control.Monad.State (MonadState, State, evalState, gets, modify)
 import Data.Map (Map)
 import qualified Data.Map as Map
@@ -19,53 +19,53 @@ import Transformations.Anf.Anf
 import Trees.Common
 
 ppRiscVAsm :: Module -> Text
-ppRiscVAsm m = Asm.ppCodeLines $ genModule m
+ppRiscVAsm m = Rv.ppCodeLines $ genModule m
 
 -- The Code
 
-type CodeGenM = Asm.AsmBuilderT (State Env)
+type CodeGenM = Rv.AsmBuilderT (State Env)
 
 data Env = Env
-  { locVars :: Map Identifier' Asm.Operand,
-    globVars :: Map Identifier' Asm.Operand,
-    funs :: Map Identifier' (Asm.Operand, Arity)
+  { locVars :: Map Identifier' Rv.Operand,
+    globVars :: Map Identifier' Rv.Operand,
+    funs :: Map Identifier' (Rv.Operand, Arity)
   }
 
-genModule :: Module -> [Asm.CodeLine]
+genModule :: Module -> [Rv.CodeLine]
 genModule (Module (Program decls)) = flip evalState (Env Map.empty Map.empty Map.empty) $
-  Asm.compileT $ do
+  Rv.compileT $ do
     mapM_ genStdLibDecl StdLib.allDeclsWithArity
     mapM_ genGlobDecl decls
 
     -- In the `main` we define our global variables.
-    Asm.mainFunction $ \_ -> mapM_ gVarDef decls
+    Rv.mainFunction $ \_ -> mapM_ gVarDef decls
   where
     gVarDef :: GlobalDeclaration -> CodeGenM ()
     gVarDef = \case
       GlobVarDecl ident value -> do
         addr <- findGlobVar ident
         value' <- genExpr value
-        Asm.storeToLabeledAddr addr value'
+        Rv.storeToLabeledAddr addr value'
       _ -> return ()
 
 genStdLibDecl :: StdLib.DeclarationWithArity -> CodeGenM ()
 genStdLibDecl decl = declareAsExtern decl >>= register decl
   where
-    declareAsExtern :: StdLib.DeclarationWithArity -> CodeGenM Asm.Operand
-    declareAsExtern (ident, _) = Asm.externFunction ident
+    declareAsExtern :: StdLib.DeclarationWithArity -> CodeGenM Rv.Operand
+    declareAsExtern (ident, _) = Rv.externFunction ident
 
-    register :: StdLib.DeclarationWithArity -> Asm.Operand -> CodeGenM ()
+    register :: StdLib.DeclarationWithArity -> Rv.Operand -> CodeGenM ()
     register (ident, arity) fun = regFun (Txt ident) fun arity
 
 genGlobDecl :: GlobalDeclaration -> CodeGenM ()
 genGlobDecl = \case
   GlobVarDecl ident _ -> do
-    var <- Asm.globalVar (Txt.pack $ genId ident)
+    var <- Rv.globalVar (Txt.pack $ genId ident)
     regGlobVar ident var
   GlobFunDecl ident params body -> mdo
     regFun ident fun (length params)
     fun <- locally $ do
-      Asm.function
+      Rv.function
         (Txt.pack $ genId ident)
         (fromIntegral $ length params)
         $ \args -> do
@@ -78,7 +78,7 @@ genId = \case
   Txt txt -> Txt.unpack txt
   Gen n txt -> Txt.unpack txt <> "_" <> show n
 
-genExpr :: Expression -> CodeGenM Asm.Operand
+genExpr :: Expression -> CodeGenM Rv.Operand
 genExpr = \case
   ExprAtom atom -> genAtom atom
   ExprComp ce -> genComp ce
@@ -87,53 +87,53 @@ genExpr = \case
     regLocVar ident val'
     genExpr expr
 
-genAtom :: AtomicExpression -> CodeGenM Asm.Operand
+genAtom :: AtomicExpression -> CodeGenM Rv.Operand
 genAtom = \case
   AtomId ident -> findAny ident
-  AtomUnit -> Asm.immediate 0
-  AtomBool bool -> Asm.immediate $ fromBool bool
-  AtomInt int -> Asm.immediate int
+  AtomUnit -> Rv.immediate 0
+  AtomBool bool -> Rv.immediate $ fromBool bool
+  AtomInt int -> Rv.immediate int
 
-genComp :: ComplexExpression -> CodeGenM Asm.Operand
+genComp :: ComplexExpression -> CodeGenM Rv.Operand
 genComp = \case
   CompApp f arg -> do
     f' <- findAny f
     arg' <- genAtom arg
     applyF <- findFun (Txt "miniml_apply")
-    Asm.call applyF [f', arg']
+    Rv.call applyF [f', arg']
   CompIte c t e -> do
     c' <- genAtom c
-    Asm.ite c' (\_ -> genExpr t) (\_ -> genExpr e)
+    Rv.ite c' (\_ -> genExpr t) (\_ -> genExpr e)
   CompBinOp op lhs rhs -> do
     lhs' <- genAtom lhs
     rhs' <- genAtom rhs
     let opF = case op of
-          BoolOp AndOp -> Asm.and
-          BoolOp OrOp -> Asm.or
-          ArithOp PlusOp -> Asm.add
-          ArithOp MinusOp -> Asm.sub
-          ArithOp MulOp -> Asm.mul
+          BoolOp AndOp -> Rv.and
+          BoolOp OrOp -> Rv.or
+          ArithOp PlusOp -> Rv.add
+          ArithOp MinusOp -> Rv.sub
+          ArithOp MulOp -> Rv.mul
           ArithOp DivOp ->
             ( \lhs'' rhs'' -> do
                 divF <- findFun (Txt "miniml_div")
-                Asm.call divF [lhs'', rhs'']
+                Rv.call divF [lhs'', rhs'']
             )
-          CompOp EqOp -> Asm.eq
-          CompOp NeOp -> Asm.ne
-          CompOp LtOp -> Asm.lt
-          CompOp LeOp -> Asm.le
-          CompOp GtOp -> Asm.gt
-          CompOp GeOp -> Asm.ge
+          CompOp EqOp -> Rv.eq
+          CompOp NeOp -> Rv.ne
+          CompOp LtOp -> Rv.lt
+          CompOp LeOp -> Rv.le
+          CompOp GtOp -> Rv.gt
+          CompOp GeOp -> Rv.ge
     opF lhs' rhs'
   CompUnOp op x -> do
     x' <- genAtom x
     let opF = case op of
-          UnMinusOp -> Asm.neg
+          UnMinusOp -> Rv.neg
     opF x'
 
 -- Vars & Funs
 
-findAny :: Identifier' -> CodeGenM Asm.Operand
+findAny :: Identifier' -> CodeGenM Rv.Operand
 findAny ident = do
   maybeLocVar <- gets ((Map.!? ident) . locVars)
   case maybeLocVar of
@@ -143,24 +143,24 @@ findAny ident = do
       case maybeFun of
         Just (fun, arity) -> do
           funToPafF <- findFun (Txt "miniml_fun_to_paf")
-          arity' <- Asm.immediate $ fromIntegral arity
-          Asm.call funToPafF [fun, arity']
+          arity' <- Rv.immediate $ fromIntegral arity
+          Rv.call funToPafF [fun, arity']
         Nothing -> findGlobVar ident
 
-findGlobVar :: (MonadState Env m) => Identifier' -> m Asm.Operand
+findGlobVar :: (MonadState Env m) => Identifier' -> m Rv.Operand
 findGlobVar ident = gets ((Map.! ident) . globVars)
 
-findFun :: Identifier' -> CodeGenM Asm.Operand
+findFun :: Identifier' -> CodeGenM Rv.Operand
 findFun ident = gets (fst . (Map.! ident) . funs)
 
-regLocVar :: (MonadState Env m) => Identifier' -> Asm.Operand -> m ()
+regLocVar :: (MonadState Env m) => Identifier' -> Rv.Operand -> m ()
 regLocVar ident var = modify $
   \env -> env {locVars = Map.insert ident var (locVars env)}
 
-regGlobVar :: (MonadState Env m) => Identifier' -> Asm.Operand -> m ()
+regGlobVar :: (MonadState Env m) => Identifier' -> Rv.Operand -> m ()
 regGlobVar ident gVar = modify $
   \env -> env {globVars = Map.insert ident gVar (globVars env)}
 
-regFun :: (MonadState Env m) => Identifier' -> Asm.Operand -> Arity -> m ()
+regFun :: (MonadState Env m) => Identifier' -> Rv.Operand -> Arity -> m ()
 regFun ident fun paramsCnt = modify $
   \env -> env {funs = Map.insert ident (fun, paramsCnt) (funs env)}
